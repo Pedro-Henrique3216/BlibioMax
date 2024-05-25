2407934efb2c10b82e890c7655ef9b97268121c6
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-25 19:01:50 -0300

    feat: initial commit

---
+++ src/main/java/com/example/bibliomax/BiblioMaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliomax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioMaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiblioMaxApplication.class, args);
+    }
+
+}
