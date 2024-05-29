ce6b6c40d93911ec94cb37b39f601ca8cf51bffd
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-28 22:59:59 -0300

    feat: add ObjectNotFoundException

---
+++ src/main/java/com/example/bibliomax/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.bibliomax.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
