3ef1ac32007d11e84dce73f5924636f8097f92f8
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-28 22:47:51 -0300

    refactor: add list of bibliotecarios

--- src/main/java/com/example/bibliomax/controller/BibliotecarioController.java
+++ src/main/java/com/example/bibliomax/controller/BibliotecarioController.java
@@ -7,9 +7,12 @@ import com.example.bibliomax.service.BibliotecarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/bibliotecario")
@@ -24,4 +27,12 @@ public class BibliotecarioController {
         bibliotecarioService.cadastrarBibliotecario(bibliotecario);
         return ResponseEntity.status(201).body(new RetornaBibliotecarioDto(bibliotecario));
     }
+
+    @GetMapping
+    public ResponseEntity<List<RetornaBibliotecarioDto>> listarBibliotecario() {
+        List<RetornaBibliotecarioDto> listaBibliotecarios = bibliotecarioService.listarBibliotecarios().stream().map(RetornaBibliotecarioDto::new).toList();
+
+        return ResponseEntity.status(200).body(listaBibliotecarios);
+    }
+
 }
--- src/main/java/com/example/bibliomax/service/BibliotecarioService.java
+++ src/main/java/com/example/bibliomax/service/BibliotecarioService.java
@@ -5,6 +5,8 @@ import com.example.bibliomax.repository.BibliotecarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BibliotecarioService {
 
@@ -13,5 +15,9 @@ public class BibliotecarioService {
 
     public Bibliotecario cadastrarBibliotecario(Bibliotecario bibliotecario) {
         return bibliotecarioRepository.save(bibliotecario);
+    }
+
+    public List<Bibliotecario> listarBibliotecarios() {
+        return bibliotecarioRepository.findAll();
     }
 }
