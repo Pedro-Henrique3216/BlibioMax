cb880b7b64bef934697b761a45c791e7e768cbc9
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-28 22:32:47 -0300

    feat: add bibliotecario repository

---
+++ src/main/java/com/example/bibliomax/repository/BibliotecarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.bibliomax.repository;
+
+import com.example.bibliomax.model.Bibliotecario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BibliotecarioRepository extends JpaRepository<Bibliotecario, Long> {
+}
