1b448762ec11b7389b7b971dd6a48ece3f8bd456
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-30 23:27:40 -0300

    refactor: add list of rented livros

--- src/main/java/com/example/bibliomax/model/Pessoa.java
+++ src/main/java/com/example/bibliomax/model/Pessoa.java
@@ -3,6 +3,9 @@ package com.example.bibliomax.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Table(name = "users")
 @Entity(name = "user")
 @NoArgsConstructor
@@ -20,5 +23,12 @@ public class Pessoa {
     private String email;
     private String senha;
     private String telefone;
+    @OneToMany
+    private List<Livro> livrosAlugados = new ArrayList<>();
 
+    public void validaSePodeAlugar(){
+        if(livrosAlugados.size() > 5){
+            throw new RuntimeException("Você não pode alugar mais livros");
+        }
+    }
 }
