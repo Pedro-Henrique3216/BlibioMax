2fa5136e48ad064f04bfdc75e70f9fd483f33439
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-08-07 22:13:44 -0300

    feat: add anotação do spring security

--- src/main/java/com/example/bibliomax/controller/BibliotecarioController.java
+++ src/main/java/com/example/bibliomax/controller/BibliotecarioController.java
@@ -2,23 +2,23 @@ package com.example.bibliomax.controller;
 
 import com.example.bibliomax.dto.RetornaBibliotecarioDto;
 import com.example.bibliomax.dto.RetornaLivroDto;
-import com.example.bibliomax.model.BibliotecarioDto;
 import com.example.bibliomax.model.Bibliotecario;
+import com.example.bibliomax.model.BibliotecarioDto;
 import com.example.bibliomax.model.Livro;
 import com.example.bibliomax.model.LivroDto;
 import com.example.bibliomax.service.BibliotecarioService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
-@Controller
+@RestController
 @RequestMapping("/bibliotecario")
 public class BibliotecarioController {
 
@@ -33,18 +33,21 @@ public class BibliotecarioController {
         return ResponseEntity.created(uri).body(new RetornaBibliotecarioDto(bibliotecario));
     }
 
+    @SecurityRequirement(name = "bearer-key")
     @GetMapping
     public ResponseEntity<Page<RetornaBibliotecarioDto>> listarBibliotecario(Pageable pageable) {
         Page<RetornaBibliotecarioDto> listaBibliotecarios = bibliotecarioService.listarBibliotecarios(pageable).map(RetornaBibliotecarioDto::new);
         return ResponseEntity.ok(listaBibliotecarios);
     }
 
+    @SecurityRequirement(name = "bearer-key")
     @GetMapping("/{id}")
     public ResponseEntity<RetornaBibliotecarioDto> buscarBibliotecarioPorId(@PathVariable Long id) {
         Bibliotecario bibliotecario = bibliotecarioService.buscarBibliotecarioPorId(id);
         return ResponseEntity.ok(new RetornaBibliotecarioDto(bibliotecario));
     }
 
+    @SecurityRequirement(name = "bearer-key")
     @PutMapping("/{id}")
     @Transactional
     public ResponseEntity<RetornaBibliotecarioDto> atualizaBibliotecario(@PathVariable Long id, @RequestBody BibliotecarioDto bibliotecarioDto) {
@@ -52,6 +55,7 @@ public class BibliotecarioController {
         return ResponseEntity.ok(new RetornaBibliotecarioDto(bibliotecario));
     }
 
+    @SecurityRequirement(name = "bearer-key")
     @PostMapping("/cadastrarLivro")
     public ResponseEntity<RetornaLivroDto> cadastraLivro(@RequestBody LivroDto livroDto, UriComponentsBuilder uriBuilder){
         Livro livro = bibliotecarioService.cadastrarLivro(livroDto);
