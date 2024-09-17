4ddf4e22c6f541d3779fb3729197db322155b047
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-17 11:09:32 -0300

    feat: alterando consulta para retorna um boolean

--- src/main/java/com/example/bibliomax/repository/LivroRepository.java
+++ src/main/java/com/example/bibliomax/repository/LivroRepository.java
@@ -2,6 +2,11 @@ package com.example.bibliomax.repository;
 
 import com.example.bibliomax.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    boolean existsByTitulo(String titulo);
 }
