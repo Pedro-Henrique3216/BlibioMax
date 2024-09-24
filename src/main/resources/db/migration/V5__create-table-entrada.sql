00a37f8848986696c4371e52af8ff94b2f70b655
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-23 21:51:01 -0300

    fix: arrumando banco de dados

--- src/main/resources/db/migration/V5__create-table-entrada.sql
+++ src/main/resources/db/migration/V5__create-table-entrada.sql
@@ -1,7 +1,8 @@
 create table entrada (
-    numero_da_nota decimal(10) primary key ,
+    numero_da_nota bigint primary key ,
     bibliotecario_id bigint,
     data_entrada DATE not null,
-    valor_total decimal(5, 2),
+    valor_total decimal,
+    status varchar(20),
     foreign key (bibliotecario_id) references bibliotecarios(id)
 );
--- src/main/resources/db/migration/V6__create-table-itens_entrada.sql
+++ src/main/resources/db/migration/V6__create-table-itens_entrada.sql
@@ -1,6 +1,7 @@
 CREATE TABLE itens_entrada (
         id bigint auto_increment primary key,
         entrada_id BIGINT not null ,
+        preco decimal(10, 2) not null,
         quantidade INT not null,
         valor_total DECIMAL(10, 2) not null,
         livro_id BIGINT not null ,
