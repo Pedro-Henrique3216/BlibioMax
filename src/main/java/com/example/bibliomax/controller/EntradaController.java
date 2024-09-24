bb83bc0a54590520036212233e8318280b4c77c8
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-23 22:06:14 -0300

    feat: add metodo para cadastrar entrada e add itens para salvar e para buscar entrada por numero da nota

---
+++ src/main/java/com/example/bibliomax/controller/EntradaController.java
@@ -0,0 +1,63 @@
+package com.example.bibliomax.controller;
+
+import com.example.bibliomax.dto.*;
+import com.example.bibliomax.model.Entrada;
+import com.example.bibliomax.model.EntradaDto;
+import com.example.bibliomax.service.EntradaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("administrativo")
+public class EntradaController {
+
+    @Autowired
+    EntradaService entradaService;
+
+    @GetMapping("/entrada/add")
+    public ResponseEntity<RetornoCadastroDto> cadastroEntrada(@RequestBody CadastroEntradaRequest cadastroEntradaRequest) {
+        Entrada entrada = entradaService.criarEntrada(cadastroEntradaRequest.entradaDto());
+        if(cadastroEntradaRequest.itensEntradaDto() != null){
+            entradaService.addItensEntrada(entrada, cadastroEntradaRequest.itensEntradaDto());
+        }
+        System.out.println(entradaService.buscarItensEntradas(entrada));
+        return ResponseEntity.ok(new RetornoCadastroDto(new RetornaEntradaDto(cadastroEntradaRequest.entradaDto().numeroNota()),
+                entradaService.buscarItensEntradas(entrada)
+                        .stream()
+                        .map(itens -> new RetornaItensDto(itens.getLivro().getId(), itens.getLivro().getTitulo(), itens.getPreco(), itens.getQuantidade(), itens.getValorTotal()))
+                        .toList()));
+    }
+
+    @PostMapping("/entrada/salvar")
+    @Transactional
+    public ResponseEntity<RetornaEntradaItensDto> salvarEntrada(@RequestBody EntradaDto entradaDto) {
+        List<RetornaItensDto> dtos = entradaService.saveItensEntrada(entradaDto);
+        Entrada entrada = entradaService.buscarEntrada(entradaDto.numeroNota());
+        System.out.println(dtos);
+        return ResponseEntity.ok(new RetornaEntradaItensDto(
+                new RetornaEntradaDto(entradaDto.numeroNota()),
+                entrada.getDataEntrada(),
+                dtos,
+                entrada.getValorTotal(),
+                entrada.getStatus()
+        ));
+    }
+
+    @GetMapping("/entrada")
+    public ResponseEntity<RetornaEntradaItensDto> buscarEntrada(@RequestParam(value = "numero_da_nota") Long numeroNota) {
+        List<RetornaItensDto> dtos = entradaService.buscaTodosItensEntradas(numeroNota);
+        Entrada entrada = entradaService.buscarEntrada(numeroNota);
+        return ResponseEntity.ok(new RetornaEntradaItensDto(
+                new RetornaEntradaDto(entrada.getNumeroNota()),
+                entrada.getDataEntrada(),
+                dtos,
+                entrada.getValorTotal(),
+                entrada.getStatus()
+        ));
+    }
+
+}
