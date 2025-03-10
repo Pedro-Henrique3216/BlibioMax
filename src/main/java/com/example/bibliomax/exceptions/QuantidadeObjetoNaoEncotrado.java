7ac684d35a9173d3b8cf5425b757af7aca617338
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-10 11:25:58 -0300

    feat: create error for insufficient stock quantity

---
+++ src/main/java/com/example/bibliomax/exceptions/QuantidadeObjetoNaoEncotrado.java
@@ -0,0 +1,9 @@
+package com.example.bibliomax.exceptions;
+
+public class QuantidadeObjetoNaoEncotrado extends RuntimeException {
+
+    public QuantidadeObjetoNaoEncotrado(String message) {
+        super(message);
+    }
+
+}
