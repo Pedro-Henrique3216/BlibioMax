0cf276f1b426853a41742dd60c62eb66d6ca4a6f
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-10 10:57:22 -0300

    feat: enable async processing and add related configuration

---
+++ src/main/java/com/example/bibliomax/config/AsyncConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.bibliomax.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfiguration {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsynchThread-");
+        executor.initialize();
+        return executor;
+    }
+}
