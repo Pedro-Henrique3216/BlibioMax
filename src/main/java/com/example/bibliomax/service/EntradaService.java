00d512a1357843bac43ce19342f582706c2e04e7
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-10 11:35:47 -0300

    feat: add verification to check if librarian is the same who created the entry

--- src/main/java/com/example/bibliomax/service/EntradaService.java
+++ src/main/java/com/example/bibliomax/service/EntradaService.java
@@ -2,10 +2,12 @@ package com.example.bibliomax.service;
 
 import com.example.bibliomax.dto.RetornaItensDto;
 import com.example.bibliomax.exceptions.EntradaJaConcluida;
+import com.example.bibliomax.exceptions.EntryOwnershipException;
 import com.example.bibliomax.exceptions.ObjetoNaoEncontrado;
 import com.example.bibliomax.model.*;
 import com.example.bibliomax.repository.EntradaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,18 +15,22 @@ import java.util.List;
 @Service
 public class EntradaService {
 
+    private final EntradaRepository repository;
+
+    private final BibliotecarioService bibliotecarioService;
+
+    private final ItensEntradaService itensEntradaService;
+
+
     @Autowired
-    private EntradaRepository repository;
+    public EntradaService(EntradaRepository repository, BibliotecarioService bibliotecarioService, ItensEntradaService itensEntradaService) {
+        this.repository = repository;
+        this.bibliotecarioService = bibliotecarioService;
+        this.itensEntradaService = itensEntradaService;
+    }
 
-    @Autowired
-    private BibliotecarioService bibliotecarioService;
-
-    @Autowired
-    private ItensEntradaService itensEntradaService;
-
-
-    public Entrada criarEntrada(EntradaDto entradaDto) {
-        Bibliotecario bibliotecario = bibliotecarioService.buscarBibliotecarioPorId(entradaDto.bibiliotecarioId());
+    public Entrada criarEntrada(EntradaDto entradaDto, UserDetails userDetails) {
+        Bibliotecario bibliotecario = bibliotecarioService.findByUsername(userDetails.getUsername());
         Entrada entrada = new Entrada(entradaDto.numeroNota(), bibliotecario);
         if(!repository.existsByNumeroNota(entradaDto.numeroNota())) {
             return repository.save(entrada);
@@ -48,8 +54,12 @@ public class EntradaService {
         return repository.findById(numeroNota).orElseThrow(() -> new ObjetoNaoEncontrado("Entrada não encontrada"));
     }
 
-    public List<RetornaItensDto> saveItensEntrada(EntradaDto entradaDto){
+    public List<RetornaItensDto> saveItensEntrada(EntradaDto entradaDto, UserDetails userDetails){
         Entrada entrada = buscarEntrada(entradaDto.numeroNota());
+        Bibliotecario bibliotecario = bibliotecarioService.findByUsername(userDetails.getUsername());
+        if (bibliotecario != entrada.getBibliotecario()){
+            throw new EntryOwnershipException("Only the librarian who initiated the entry can save it.");
+        }
         itensEntradaService.saveItensEntrada(entrada);
         entrada.setStatus(StatusEntrada.CONCLUIDO);
         return itensEntradaService.buscaTodosItensPorEntrada(entrada);
